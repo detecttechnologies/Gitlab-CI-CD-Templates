7c4c2e9e57e7973f52c3139c4fd7012e4ef11be6
avinashtrivedi11 <116353157+avinashtrivedi11@users.noreply.github.com>
2023-07-13 07:25:06 +0000

    bugfix:deleting files from destination before pushing

--- compile/python/build-binaries.sh
+++ compile/python/build-binaries.sh
@@ -1,44 +1,37 @@
 #!/bin/bash
 
-# Create directory for loading files
-mkdir -p /app/user/load
+LOAD_DIR="/app/user/load"
+CWD=$(pwd)
+ARTIFACTS_DIR="${CWD}/artifacts"
+
+# Create directories, copy all files to load directory
+mkdir -p -v ${LOAD_DIR} ${ARTIFACTS_DIR}
 
 # Copy all files to the loading directory
-cp -R ./* /app/user/load
-
-# Navigate to the loading directory
-cd /app/user/load
+cp -R ./* ${LOAD_DIR}
+echo "Copied repo files to load directory for compilation."
 
 # Check if the variable $EXCLUDE_FILES is defined
-if [ -z "$EXCLUDE_FILES" ]; then
-  echo "EXCLUDE_FILES is not defined"
+if [ -z "${EXCLUDE_FILES}" ]; then
+  echo "No files to be excluded from compilation"
 else
-  # Exclude any python file from cythonization
   IFS=$'\n' read -rd '' -a exclude_files <<<"$EXCLUDE_FILES"
   for file in "${exclude_files[@]}"
   do
-    find . -path "$file" -prune -exec rm -rv "{}" \;
+    file=${file#./}
+    find ${LOAD_DIR} -type f -wholename "*$file*" -exec rm "{}" \;
+    echo "Removed ${file} from compilation"
   done
 fi
 
-# Navigate back to the previous directory
-cd -
-
-# Navigate to the root directory
-cd /app
-
-# Compile python files using py_compiler.py
-python3 py_compiler.py build_ext -b /app/user/load
-
-# Navigate back to the previous directory
-cd -
-
-# Create directory for compiled files
-cwd=$(pwd)
-mkdir artifacts
-
-# Navigate to the loading directory
-cd /app/user/load/
+# Compile python files
+echo "Starting Compilation"
+echo "--------------------------------------------------------"
+python3 /app/py_compiler.py build_ext -b ${LOAD_DIR}
+echo "--------------------------------------------------------"
+echo "Finished Compilation"
 
 # Copy all compiled .so files to the artifacts directory
-find . -wholename "**/*.so" -exec cp -v --parents \{\} $cwd/artifacts \;
+echo "Copying .so files to artifacts"
+cd ${LOAD_DIR}
+find . -wholename "**/*.so" -exec cp --parents \{\} ${ARTIFACTS_DIR} \;
--- compile/python/push-binaries.sh
+++ compile/python/push-binaries.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Setup Credentials
-git config --global user.email "PlatformTeam@detecttechnologies.com"
-git config --global user.name "Detect Gitlab Bot"
-
 # Copy files from artifacts to their original distination and remove artifacts folder
 cwd=$(pwd)
 cd artifacts
-find . -name "**" -exec cp -Rv --parents \{\} $cwd/ \;
+find . -name "**" -exec cp -R --parents \{\} $cwd/ \;
 cd -
 rm -rf artifacts
 
@@ -27,10 +23,8 @@ git clone "https://oauth2:$BOT_ACCESS_TOKEN@gitlab.com/${BUILD_OUTPUT_REPO}" /ro
 
 # Remove branches from remote if the variable is assigned "true"
 cd /root/dest
-if [[ $REMOVE_BRANCHES == "true" ]]
-then
-    git branch -r | awk -F/ '/\/linux/{print $2}' | xargs -I {} git push origin :{};
-fi
+echo "Removing branches from destination repo for pushing latest version"
+git branch -r | awk -F/ '/\/linux/{print $2}' | xargs -I {} git push origin :{};
 cd -
 
 # Setup a branch for every unique deployment footprint, push to each of them
@@ -41,30 +35,20 @@ echo "Branches to be pushed: ${branch_names}"
 
 for branch in ${branch_names}
 do  
+    cd /root/dest
+    git checkout --quiet main
     echo "-------------------------------------------"
     echo "Now pushing branch ${branch}"
-    cd /root/dest
-    
+
     # Switch to the branch (create if it doesn't exist)
     git checkout --quiet ${branch} 2>/dev/null || git checkout -b ${branch}
-
-    #Delete all files before pushing if any
-    git rm -rf * || true
-    git commit -m "removing previous builds" || true
-    git push --quiet origin HEAD:${branch} -f || true
-    cd -
     
-    # From the build folder, copy the new .so files, main.py and all __init__.py's;
+    cd -
     
     file_name_pattern=$(echo ${branch} | awk -F '-' '{print $3"-"$2"-"$1}') # Undo the reversing carried out while forming the branch_name
     file_name_pattern=${file_name_pattern:2}   # Remove the 'py' at the start;
-    find . -wholename "**/*${file_name_pattern}*.so" ! -wholename "./iva.**.so" -exec cp --parents \{\} /root/dest/ \;
-    cp ./main.py /root/dest/ || true
-    find . -wholename "**/__init__.py" -exec cp --parents \{\} /root/dest/ \;
 
-    # IVA repo specific code
-    cp ./iva.py /root/dest/ || true
-
+    echo "Copying files mentioned in COPY_FILES"
     # From the build folder, copy the files and folders mentioned in variable "COPY_FILES"
     for file in ${COPY_FILES}
     do
@@ -75,15 +59,34 @@ do
             echo "${split[1]}"
             cp -R -v ${split[0]} /root/dest/${split[1]}
         else
-            find . -type f -wholename "*$file*" ! -wholename "./*.git*" -exec cp -v -R --parents \{\} /root/dest/ \;
+            find . -type f -wholename "*$file*" ! -wholename "./*.git*" -exec cp --parents \{\} /root/dest/ \;
         fi
     done
 
+    # Remove .so files that may have been copied due to presence in COPY_FILES from destination
+    cd /root/dest
+    find . -type f -wholename "*cpython*.so" -exec rm -v "{}" \; 
+    cd -
+    
+    # From the build folder, copy the new .so files, main.py and all __init__.py's;
+    echo "Copying .so files"   
+    find . -wholename "**/*${file_name_pattern}*.so" ! -wholename "./iva.**.so" -exec cp --parents \{\} /root/dest/ \;
+    
+    echo "Copying main.py"
+    cp -v ./main.py /root/dest/ || true
+    # IVA repo specific code
+    cp -v ./iva.py /root/dest/ || true
+
+    echo "Copying __init__ files"
+    find . -wholename "**/__init__.py" -exec cp --parents \{\} /root/dest/ \;
+
+
     #Rename filenames to original names
     cd /root/dest
+    echo "Renaming .so files"
     for f in $(find . -type f -wholename "./**.so") 
     do
-        mv -v "$f" "${f/.cpython-${file_name_pattern}-gnu./.}" || mv -v "$f" "${f/.cpython-${file_name_pattern}-gnueabihf./.}"
+        mv "$f" "${f/.cpython-${file_name_pattern}-gnu./.}" || mv "$f" "${f/.cpython-${file_name_pattern}-gnueabihf./.}"
     done
     cd -
 
@@ -93,8 +96,7 @@ do
     # Push back the updated repository
     cd /root/dest
     
-    git add -A && git commit -m "Commit in source pipeline at timestamp:$timestamp" --allow-empty
-    git push --quiet origin HEAD:${branch} -f
+    git add -A && git commit -m "Commit in source pipeline at timestamp:$timestamp"
     
     cd -
     # Finally copy the .gitignore
@@ -102,13 +104,10 @@ do
     
     cd /root/dest
     git add -A && git commit -m "Commit in source pipeline at timestamp:$timestamp" --allow-empty
-    git reflog expire --expire-unreachable=now --all
-    git gc --prune=now
-    git push --quiet origin HEAD:${branch} -f
 
     # Merge all commits in one
     git reset $(git commit-tree HEAD^{tree} -m "Commit in source pipeline at timestamp:$timestamp")
     git push --quiet origin HEAD:${branch} -f
     
     cd - # Switch back to the source repo before the next iteration
-    done
+done
