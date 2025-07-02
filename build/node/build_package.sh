699d77180e75fc964048b1e1b2745109d7b8aa84
avinashtrivedi11 <116353157+avinashtrivedi11@users.noreply.github.com>
2025-07-02 22:12:08 +0000

    👷 updated build rules for node package pipeline and removed redundant build using latest tag

--- build/node/build_package.sh
+++ build/node/build_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Building npm package for project: ${CI_PROJECT_NAME} with tag: ${VERSION_TAG}"
+echo "Building npm package for project: ${CI_PROJECT_NAME} with tag: ${VERSION}"
 apt-get update && apt-get install -y jq
 # Main build logic
 if [ "$CI_PROJECT_NAME" != "tpulse-msa-fe-root-app" ]; then
@@ -8,7 +8,7 @@ if [ "$CI_PROJECT_NAME" != "tpulse-msa-fe-root-app" ]; then
   npm ci
   npm run build
   echo "Creating a distributable tarball of the dist directory"
-  tar -czf "${CI_PROJECT_NAME}-${VERSION_TAG}.tar.gz" -C dist .
+  tar -czf "${CI_PROJECT_NAME}-${VERSION}.tar.gz" -C dist .
 else
   # For root_repo, skip npm steps and create tarball without .git files
   echo "Building root application..."
@@ -16,7 +16,7 @@ else
   echo "Creating a tarball for root_repo without .git files"
   mkdir temp_dir
   rsync -av --exclude='.git' --exclude='temp_dir' --exclude='.gitlab-ci.yml' --exclude='.vscode' ./ temp_dir/
-  tar -czf "${CI_PROJECT_NAME}-${VERSION_TAG}.tar.gz" -C temp_dir .
+  tar -czf "${CI_PROJECT_NAME}-${VERSION}.tar.gz" -C temp_dir .
   rm -rf temp_dir
 fi
 
@@ -28,11 +28,11 @@ EXISTING_PACKAGE_ID=$(
   curl --silent \
     --header "JOB-TOKEN: $CI_JOB_TOKEN" \
     "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages?package_type=generic&package_name=${CI_PROJECT_NAME}" \
-    | jq -r ".[] | select(.version == \"${VERSION_TAG}\").id"
+    | jq -r ".[] | select(.version == \"${VERSION}\").id"
 );
-echo "Existing package ID for ${VERSION_TAG}: $EXISTING_PACKAGE_ID";
+echo "Existing package ID for ${VERSION}: $EXISTING_PACKAGE_ID";
 if [ -n "$EXISTING_PACKAGE_ID" ]; then
-  echo "Deleting old package files for package ID $EXISTING_PACKAGE_ID for ${VERSION_TAG}...";
+  echo "Deleting old package files for package ID $EXISTING_PACKAGE_ID for ${VERSION}...";
   PACKAGE_FILE_IDS=$(
     curl --silent \
       --header "JOB-TOKEN: $CI_JOB_TOKEN" \
@@ -47,46 +47,7 @@ if [ -n "$EXISTING_PACKAGE_ID" ]; then
 fi
 
 # 3b) Upload the new release.zip for the commit tag
-echo "Uploading package with version tag: ${VERSION_TAG}"
+echo "Uploading package with version tag: ${VERSION}"
 curl --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-     --upload-file "${CI_PROJECT_NAME}-${VERSION_TAG}.tar.gz" \
-     "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/generic/${CI_PROJECT_NAME}/${VERSION_TAG}/${CI_PROJECT_NAME}-${VERSION_TAG}.tar.gz"
-
-# 4) Handle the "latest" tag package only if CI_COMMIT_TAG is present
-if [ -n "$CI_COMMIT_TAG" ]; then
-  echo "Handling 'latest' tag package..."
-
-  # 4a) Find any existing package with the "latest" version
-  EXISTING_PACKAGE_ID_LATEST=$(
-    curl --silent \
-      --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-      "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages?package_type=generic&package_name=${CI_PROJECT_NAME}" \
-      | jq -r ".[] | select(.version == \"latest\").id"
-  );
-  echo "Existing package ID for latest: $EXISTING_PACKAGE_ID_LATEST";
-  if [ -n "$EXISTING_PACKAGE_ID_LATEST" ]; then
-    echo "Deleting old package files for package ID $EXISTING_PACKAGE_ID_LATEST for latest...";
-    PACKAGE_FILE_IDS_LATEST=$(
-      curl --silent \
-        --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-        "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/${EXISTING_PACKAGE_ID_LATEST}/package_files" \
-        | jq -r ".[].id"
-    );
-    for FILE_ID in $PACKAGE_FILE_IDS_LATEST; do
-      curl --request DELETE \
-           --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-           "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/${EXISTING_PACKAGE_ID_LATEST}/package_files/${FILE_ID}";
-    done
-  fi
-
-  # 4b) Upload the new release.zip for the "latest" tag
-  echo "Uploading package with latest tag"
-  cp "${CI_PROJECT_NAME}-${VERSION_TAG}.tar.gz" "${CI_PROJECT_NAME}-latest.tar.gz"
-  curl --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-       --upload-file "${CI_PROJECT_NAME}-latest.tar.gz" \
-       "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/generic/${CI_PROJECT_NAME}/latest/${CI_PROJECT_NAME}-latest.tar.gz"
-
-  echo "'Latest' tag package uploaded successfully!"
-else
-  echo "CI_COMMIT_TAG is not present, skipping 'latest' tag handling."
-fi
+     --upload-file "${CI_PROJECT_NAME}-${VERSION}.tar.gz" \
+     "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/packages/generic/${CI_PROJECT_NAME}/${VERSION}/${CI_PROJECT_NAME}-${VERSION}.tar.gz"
